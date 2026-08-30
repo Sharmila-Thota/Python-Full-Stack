22a7c428de66c99fc74b8424004a206a0478d920
SharmilaThota <sharmilathota3@gmail.com>
2026-08-30 20:59:29 +0530

    Assigning values to variables

---
+++ Day3.py
@@ -0,0 +1,10 @@
+#Assigning multiple values to multiple variables
+a,b,c=5,10,15
+print("Value of a:",a)
+print("Value of b:",b)
+print("Value of c:",c)
+#Assign One Value to Multiple Variables
+p=q=r=100
+print("Value of p:",p)
+print("Value of q:",q)
+print("Value of r:",r)
