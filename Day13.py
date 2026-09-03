b54e89de774588eaf6fea24fe9d4c54697b74dd9
SharmilaThota <sharmilathota3@gmail.com>
2026-09-03 16:01:49 +0530

    Control Statements

--- Day13.py
+++ Day13.py
@@ -1,5 +1,58 @@
 #Conditional statements
+
+#if
 a = 10
 if a > 5:
     print("Hello")
-    
+
+#if - else
+num = 16
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+#elif
+a=19
+b=20
+c=50
+if a>b and a>c:
+    print("a is largest")
+elif b > a and b > c:
+    print("b is largest")
+else:
+    print("c is largest")
+
+#Nested if
+age =20
+weight = 53
+if age >=18:
+    if weight >= 50:
+        print("eligible")
+    else:
+        print(" not eligible")
+else:
+    print("not eligible")
+
+#Loops
+
+#for loop
+l=[1,2,3,4,5]
+for i in l:
+    print(i)
+
+#Jumping statements
+for i in range(1,6,1):
+    if i == 4:
+        break
+    print(i)
+
+#continue
+
+for i in range(1,6,1):
+    if i == 4:
+        continue
+    print(i)
+
+
+
